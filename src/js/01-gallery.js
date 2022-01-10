bf9d662ad8c21536fee7186b358eaf06d13c7053
AlexeiKokarev <87031896+AlexeiKokarev@users.noreply.github.com>
2022-01-10 15:57:17 +0200

    remake1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,29 +1,20 @@
-
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 
-const containerEl = document.querySelector('.gallery');
+const galleryEl = document.querySelector(".gallery");
 
-const markup = galleryItems
-  .map(element => {
-    return `<div class="gallery__item">
-    <a class="gallery__item" href="${element.original}">
-    <img class="gallery__image"
-    src="${element.preview}"
-     alt="${element.description}" 
-     title = "${element.description}" 
-    captionDelay = 250ms>
-    </a>
-    </div>`;
-  })
-  .join('');
-containerEl.insertAdjacentHTML('afterbegin', markup);
+const galleryMarkup = galleryItems
+    .map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" />
+</a>`
+    })
+    .join('');
 
-const gallery = new SimpleLightbox('.gallery__item a');
-gallery.on('show.simplelightbox', function () {
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkup);
 
-});
-
-console.log(galleryItems);
+new SimpleLightbox('.gallery a', { captionType: 'attr', captionDelay: 250, captionsData: 'alt' });
