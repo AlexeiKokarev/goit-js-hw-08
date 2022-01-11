1d9ac871fd6513898d8ea6af24484c1ce648e2ce
AlexeiKokarev <87031896+AlexeiKokarev@users.noreply.github.com>
2022-01-11 17:00:07 +0200

    ers

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,16 +5,20 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryEl = document.querySelector(".gallery");
 
-const galleryMarkup = galleryItems
-    .map(({ preview, original, description }) => {
-        return `<a class="gallery__item" href="${original}">
-  <img class="gallery__image" 
-  src="${preview}" 
-  alt="${description}" />
-</a>`
+const markup = galleryItems
+    .map(element => {
+        return `<div class="gallery__item">
+    <a class="gallery__item" href="${element.original}">
+    <img class="gallery__image"
+    src="${element.preview}"
+     alt="${element.description}" 
+     title = "${element.description}" 
+    captionDelay = 250ms>
+    </a>
+    </div>`;
     })
     .join('');
 
-galleryEl.insertAdjacentHTML("afterbegin", galleryMarkup);
+galleryEl.insertAdjacentHTML("afterbegin", markup);
 
 new SimpleLightbox('.gallery a', { captionType: 'attr', captionDelay: 250, captionsData: 'alt' });
