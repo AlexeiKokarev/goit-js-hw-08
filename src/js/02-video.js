6dc1f05fea787ba7e36a1b9285b3f1ef15396fae
AlexeiKokarev <87031896+AlexeiKokarev@users.noreply.github.com>
2022-01-11 17:07:57 +0200

    ers22

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,14 +3,14 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const needTime = locStor.getItem('videoplayer-current-time');
+const needTime = localStorage.getItem('videoplayer-current-time');
 
 if (needTime) {
     player.setCurrentTime(needTime);
 }
 
 function playerSetTimeUpdate(data) {
-    locStor.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
 }
 
 player.on('timeupdate', throttle(playerSetTimeUpdate, 1000));
